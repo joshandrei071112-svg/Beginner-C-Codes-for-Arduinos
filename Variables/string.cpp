49cc94906cf1a6178495ad5dc795656bd6458866
joshandrei071112-svg <joshandrei071112@gmail.com>
2026-03-27 18:38:28 +0800

    Add string array and print loop in string.cpp
    This code demonstrates the use of string arrays in C++. It initializes an array of strings and prints each string along with the next string in the array to the serial monitor, including a wrap-around mechanism.

--- Variables/string.cpp
+++ Variables/string.cpp
@@ -1,1 +1,15 @@
+char *myStrings[] = {"Josh", "Andrei", "F",
+                     "Dela Cruz", "From", "San Miguel Hagonoy Bulacan"};
 
+    void setup() {
+      Serial.begin(9600);
+    }
+
+    void loop() {
+      for (int i = 0; i < 6; i++) {
+        Serial.println(myStrings[i]);
+        Serial.println(myStrings[(i + 1) % 6]); // Print the next string, wrap around to the first string after the last one
+        delay(5000);
+      }
+    }
+// This code demonstrates the use of string arrays in C++. It initializes an array of strings and prints each string along with the next string in the array to the serial monitor. The code also includes a wrap-around mechanism to print the first string after reaching the last one.
